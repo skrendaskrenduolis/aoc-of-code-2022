6da7a6750ea0aaf116327e0161c163607fcd37b2
marty <skrenda.skrenduolis@gmail.com>
2022-12-03 14:32:12 +0100

    feat: formatting updates for day 1, 2, 3

--- day1/aoc_1.py
+++ day1/aoc_1.py
@@ -1,5 +1,5 @@
-#DAY 1
-#part 1. Finding elf with most calories
+# DAY 1
+# part 1. Finding elf with most calories
 elf_list = []
 calorie_counter = 0
 elf_counter = 0
@@ -17,11 +17,12 @@ with open('words.txt', 'r') as infile:
     elf_list.append((elf_counter, calorie_counter))
     calorie_counter = 0
 
-sorted_elf_list_by_cals = sorted(elf_list, key = lambda item:item[1], reverse=True)
+sorted_elf_list_by_cals = sorted(
+    elf_list, key=lambda item: item[1], reverse=True)
 print(f"Elf with most calories: {sorted_elf_list_by_cals[0][0]}")
 print(f"Number of calories on elf: {sorted_elf_list_by_cals[0][1]}")
 
-#part 2. total calories of top 3 elves
+# part 2. total calories of top 3 elves
 top_three_cal_total = 0
 for elf, calories in sorted_elf_list_by_cals[:3]:
     top_three_cal_total += calories
--- day3/aoc_3.py
+++ day3/aoc_3.py
@@ -1,6 +1,6 @@
 priority_string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-#Part 1
+# Part 1
 sum_of_priorities = 0
 with open('aoc_3_input.txt', 'r') as infile:
     for line in infile:
@@ -11,13 +11,13 @@ with open('aoc_3_input.txt', 'r') as infile:
                 if line[i] == line[j]:
                     duplicate_letter = line[i]
                     break
-    
+
         sum_of_priorities += (priority_string.index(duplicate_letter)+1)
 
     print(sum_of_priorities)
 
 
-#Part 2
+# Part 2
 sum_of_priorities = 0
 triple_set_list = []
 with open('aoc_3_input.txt', 'r') as infile:
@@ -25,8 +25,8 @@ with open('aoc_3_input.txt', 'r') as infile:
         triple_set_list.append(set(line.strip()))
 
         if len(triple_set_list) == 3:
-            result = ''.join(triple_set_list[0].intersection(triple_set_list[1], triple_set_list[2]))
+            result = ''.join(triple_set_list[0].intersection(
+                triple_set_list[1], triple_set_list[2]))
             sum_of_priorities += (priority_string.index(result)+1)
             triple_set_list.clear()
     print(sum_of_priorities)
-
